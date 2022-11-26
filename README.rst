0631d93b3323943e8a8310704d53cf4092aa8b8e
fiddir <jan.friedrich@uni-oldenburg.de>
2022-11-26 16:41:55 +0100

    Added some few error messages; fixed sigma of interpolating process

--- README.rst
+++ README.rst
@@ -1,8 +1,7 @@
 fBb
-==========
+===
 
-A python package for stochastic interpolation of sparse time
-or incomplete time series by fractional Brownian motion.
+A python package for stochastic interpolation of sparse or incomplete time series by fractional Brownian motion.
 
 For further references, see: J. Friedrich, S. Gallon, A. Pumir, and R. Grauer, Phys. Rev. Lett. 125, 170602 (2020).
 
@@ -16,10 +15,10 @@ The ``fBb`` package is available on pypi and can be installed using pip
     pip install fBb
 
 How to use this package
---------------
+-----------------------
 
 Stochastic interpolation
-~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~
 
 To use ``fBb`` to interpolate sparse measurement points ``X_i`` at 
 times ``t_i``
@@ -33,3 +32,8 @@ Hurst parameter ``H`` and the desired resolution of the interpolation ``N_fine``
     mfbb = MFBB(X_i, t_i, H, N_fine)
     X_bridge = mfbb.bridge()
     t_bridge = mfbb.t_fine()
+
+    plt.scatter(t_i, X_i)
+    plt.plot(t_bridge, X_bridge)
+    plt.show()
+
--- fBb/fBb.py
+++ fBb/fBb.py
@@ -10,32 +10,45 @@ import numpy as np
 from stochastic.processes.continuous import FractionalBrownianMotion
 
 class MFBB:
-    def __init__(self, X_i, t_i, H, N_fine): 
+    def __init__(self, X_i, t_i, H, N_fine, sigma=None): 
         self.X_i = X_i
         self.t_i = t_i
         self.H = H
         self.N_fine = N_fine
         self.N_coarse = np.size(X_i)
+        if self.N_fine%self.N_coarse != 0:
+            raise ValueError("Fine resolution N_fine must be multiples of number of prescribed points N_coarse")
+        if sigma is None:
+            self.sigma = np.sqrt(2*np.mean(self.X_i**2))/t_i[-1]**(H)
+        else:
+            self.sigma = sigma
         fbm = FractionalBrownianMotion(hurst=self.H, t=self.t_i[-1])
         self.X = fbm.sample(self.N_fine)
         self.t = fbm.times(self.N_fine)
-        #self.t_i = self.t[::self.N_fine//self.N_coarse]
         
     def cov(self, t, t_prime):
         T, T_prime = np.meshgrid(t, t_prime)
-        return (T**(2*self.H)+T_prime**(2*self.H)-np.abs(T-T_prime)**(2*self.H))/2.
+        return self.sigma**2*(T**(2*self.H)+T_prime**(2*self.H)-np.abs(T-T_prime)**(2*self.H))/2.
     
     def bridge(self):
+        if 0. in self.t_i:
+            raise ValueError("Please avoid zeros in t_i-array as it leads to singular inverse matrix in bridge construction.")
         coarse_points  = np.arange(1,self.N_coarse+1)*self.N_fine//self.N_coarse
         X_bridge = self.X - (self.X[coarse_points]-self.X_i)@np.linalg.inv(self.cov(self.t_i,self.t_i))@self.cov(self.t, self.t_i)
         return X_bridge
     
+    def free(self):
+        return self.X
+        
     def t_fine(self):
         return self.t
     
     def t_coarse(self):
         return self.t_i
-    
-    #def hurst(self):
-    #    return self.bridge()
+
+
+
+
+
+
 
